72ee679e08076d2422609c7a990be7b62fab781b
gpfeff <greg.a.pfeffer@gmail.com>
2014-01-03 00:09:25 -0500

    Add Name Back to Statuses Table

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140103040756) do
+ActiveRecord::Schema.define(version: 20140103050726) do
 
   create_table "statuses", force: true do |t|
     t.integer  "weight"
-    t.string   "content"
+    t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "name"
   end
 
   add_index "statuses", ["user_id"], name: "index_statuses_on_user_id"
