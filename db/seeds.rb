7ee3586183723c4828cb61f4ea4a08d864ae60a8
gpfeff <greg.a.pfeffer@gmail.com>
2014-01-05 10:55:20 -0500

    Create seed files for Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+bob = User.create (first_name:'Bob',last_name:'Smith',profile_name:'bobsmith123',email:'bobsmith123@gmail.com')
+john = User.create (first_name:'John',last_name:'Jones',profile_name:'johnjones456',email:'johnjones456@gmail.com')
+chris = User.create (first_name:'Chris',last_name:'Angle',profile_name:'chrisangle119',email:'chrisangle119@gmail.com')
+jimmy = User.create (first_name:'Jimmy',last_name:'John',profile_name:'jimmyjohn042',email:'jimmyjohn042@gmail.com')
